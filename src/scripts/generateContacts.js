3f0f4889188cbdf0368ad1893aa5ef2c8c9038a1
Volodymyr <Volodumurvel@gmail.com>
2024-05-15 17:23:57 +0200

    add main

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,41 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { PATH_DB } from "../constants/contacts.js";
+import { createFakeContact } from "../utils/createFakeContact.js";
+import fs from "fs/promises";
 
-const generateContacts = async (number) => {};
+console.log(PATH_DB);
+const generateContacts = async (number) => {
+  const dataFile = await readDataFromFile(PATH_DB);
+  const dbContacts = JSON.parse(dataFile);
+  console.log(dbContacts.length);
+  if (dbContacts.length) {
+    console.log("Database is not empty");
+    number = +dbContacts.length;
+  }
+  for (let i = 0; i < number; i++) {
+    const contact = createFakeContact();
+    writeDataToFile(PATH_DB, JSON.stringify(contact));
+    console.log(contact);
+  }
+};
 
 await generateContacts(5);
+
+async function readDataFromFile(path) {
+  try {
+    const data = fs.readFile(path, "utf-8");
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function writeDataToFile(path, data) {
+  try {
+    await fs.writeFile(path, data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+function swap() {
+  var e = n.split(" ").map(Uu).join(" ");
+  r(e);
+}
